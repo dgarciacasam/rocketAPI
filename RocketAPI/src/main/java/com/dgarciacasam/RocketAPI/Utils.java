3b1c6ca70bd228e9226c98073354979c97f7ca1a
saddxni <dgarciacasam@gmail.com>
2024-06-09 18:40:47 +0200

    Creamos nueva build del proyecto

--- RocketAPI/src/main/java/com/dgarciacasam/RocketAPI/Utils.java
+++ RocketAPI/src/main/java/com/dgarciacasam/RocketAPI/Utils.java
@@ -11,13 +11,27 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Iterator;
+import java.util.stream.Stream;
 
 @UtilityClass
 public class Utils {
     public String getProfileImages(Integer userId) throws IOException {
-        Path imagePath = Paths.get("RocketAPI","src","main","resources","static","images","profile", userId + ".jpg");
-        if(!Files.exists(imagePath)){
-            imagePath = Paths.get("RocketAPI","src","main","resources","static","images","profile", 0 + ".jpg");
+        Path profilePath = Paths.get("RocketAPI", "src", "main", "resources");
+
+        try (Stream<Path> paths = Files.walk(profilePath)) {
+            paths.forEach(path -> {
+                System.out.println(path);
+                if (Files.isRegularFile(path)) {
+                    System.out.println("Es archivo");
+                } else if (Files.isDirectory(path)) {
+                    System.out.println("Es directorio");
+                }
+            });
+        }
+
+        Path imagePath = Paths.get("RocketAPI", "src", "main", "resources", "static", "images", "profile", userId + ".jpg");
+        if (!Files.exists(imagePath)) {
+            imagePath = Paths.get("RocketAPI", "src", "main", "resources", "static", "images", "profile", "0.jpg");
         }
         byte[] imageBytes = Files.readAllBytes(imagePath);
         String imageBase64 = Base64.getEncoder().encodeToString(imageBytes);
