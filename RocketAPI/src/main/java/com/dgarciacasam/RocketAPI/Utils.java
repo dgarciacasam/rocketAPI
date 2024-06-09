6308f480450f69e4b3a5ae6dee4ddd7f2a673abd
saddxni <dgarciacasam@gmail.com>
2024-06-09 19:30:47 +0200

    Test

--- RocketAPI/src/main/java/com/dgarciacasam/RocketAPI/Utils.java
+++ RocketAPI/src/main/java/com/dgarciacasam/RocketAPI/Utils.java
@@ -1,22 +1,26 @@
 package com.dgarciacasam.RocketAPI;
 
-import com.dgarciacasam.RocketAPI.Projects.Model.ProjectModel;
-import com.dgarciacasam.RocketAPI.Tasks.Model.Task;
-import com.dgarciacasam.RocketAPI.Users.Model.User;
 import lombok.experimental.UtilityClass;
+import java.io.File;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 import java.util.Base64;
-import java.util.Iterator;
-import java.util.stream.Stream;
+
 
 @UtilityClass
 public class Utils {
     public String getProfileImages(Integer userId) throws IOException {
+        String currentPath = Paths.get("").toAbsolutePath().toString();
+        File currentDir = new File(currentPath);
+
+        System.out.println("Listing files in directory: " + currentDir.getAbsolutePath());
+        listFilesInDirectoryRecursively(currentDir, 0);
+
+
         Path imagePath = Paths.get("src", "main", "resources", "static", "images", "profile", userId + ".jpg");
         if (!Files.exists(imagePath)) {
             imagePath = Paths.get( "src", "main", "resources", "0.jpg");
@@ -24,5 +28,24 @@ public class Utils {
         byte[] imageBytes = Files.readAllBytes(imagePath);
         String imageBase64 = Base64.getEncoder().encodeToString(imageBytes);
         return imageBase64;
+    }
+
+    private void listFilesInDirectoryRecursively(File directory, int depth) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                printIndented(file.getName(), depth);
+                if (file.isDirectory()) {
+                    listFilesInDirectoryRecursively(file, depth + 1);
+                }
+            }
+        }
+    }
+
+    private void printIndented(String name, int depth) {
+        for (int i = 0; i < depth; i++) {
+            System.out.print("  ");
+        }
+        System.out.println(name);
     }
 }
