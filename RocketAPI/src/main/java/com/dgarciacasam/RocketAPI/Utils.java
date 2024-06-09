4a8e0f6c940b3f3db32937149a95e2554721468b
saddxni <dgarciacasam@gmail.com>
2024-06-09 20:16:10 +0200

    Test

--- RocketAPI/src/main/java/com/dgarciacasam/RocketAPI/Utils.java
+++ RocketAPI/src/main/java/com/dgarciacasam/RocketAPI/Utils.java
@@ -1,9 +1,13 @@
 package com.dgarciacasam.RocketAPI;
 
 import lombok.experimental.UtilityClass;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
+
 import java.io.File;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,38 +18,20 @@ import java.util.Base64;
 @UtilityClass
 public class Utils {
     public String getProfileImages(Integer userId) throws IOException {
-        String currentPath = Paths.get("").toAbsolutePath().toString();
-        File currentDir = new File(currentPath);
-
-        System.out.println("Listing files in directory: " + currentDir.getAbsolutePath());
-        listFilesInDirectoryRecursively(currentDir, 0);
-
-
-        Path imagePath = Paths.get("src", "main", "resources", "static", "images", "profile", userId + ".jpg");
-        if (!Files.exists(imagePath)) {
-            imagePath = Paths.get( "src", "main", "resources", "0.jpg");
-        }
-        byte[] imageBytes = Files.readAllBytes(imagePath);
-        String imageBase64 = Base64.getEncoder().encodeToString(imageBytes);
-        return imageBase64;
-    }
-
-    private void listFilesInDirectoryRecursively(File directory, int depth) {
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                printIndented(file.getName(), depth);
-                if (file.isDirectory()) {
-                    listFilesInDirectoryRecursively(file, depth + 1);
-                }
+        ClassPathResource imgFile = new ClassPathResource("/images/profile/" + userId + ".jpg");
+        if (!imgFile.exists()) {
+            imgFile = new ClassPathResource("/images/0.jpg");
+            if (!imgFile.exists()) {
+                return "";
             }
+
+        }
+
+        try (InputStream inputStream = imgFile.getInputStream()) {
+            byte[] imageBytes = StreamUtils.copyToByteArray(inputStream);
+            String imageBase64 = Base64.getEncoder().encodeToString(imageBytes);
+            return imageBase64;
         }
     }
 
-    private void printIndented(String name, int depth) {
-        for (int i = 0; i < depth; i++) {
-            System.out.print("  ");
-        }
-        System.out.println(name);
-    }
 }
