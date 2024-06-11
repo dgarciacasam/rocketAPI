db8469f691de217f0e0a080478fdb17865a71665
saddxni <dgarciacasam@gmail.com>
2024-06-11 16:32:44 +0200

    Modificada función para recoger las imagenes de los usuarios

--- RocketAPI/src/main/java/com/dgarciacasam/RocketAPI/Utils.java
+++ RocketAPI/src/main/java/com/dgarciacasam/RocketAPI/Utils.java
@@ -7,7 +7,6 @@ import org.springframework.util.StreamUtils;
 import java.io.File;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,20 +17,18 @@ import java.util.Base64;
 @UtilityClass
 public class Utils {
     public String getProfileImages(Integer userId) throws IOException {
-        ClassPathResource imgFile = new ClassPathResource("/images/profile/" + userId + ".jpg");
-        if (!imgFile.exists()) {
-            imgFile = new ClassPathResource("/images/0.jpg");
-            if (!imgFile.exists()) {
-                return "";
-            }
-
+        //Este código funciona en local
+        Path imagePath = Paths.get("static", "images", "profile", userId + ".jpg");
+        if (!Files.exists(imagePath)) {
+            imagePath = Paths.get("static", "images", "profile", "0.jpg");
         }
+        byte[] imageBytes = Files.readAllBytes(imagePath);
+        String imageBase64 = Base64.getEncoder().encodeToString(imageBytes);
 
-        try (InputStream inputStream = imgFile.getInputStream()) {
-            byte[] imageBytes = StreamUtils.copyToByteArray(inputStream);
-            String imageBase64 = Base64.getEncoder().encodeToString(imageBytes);
-            return imageBase64;
-        }
+        return imageBase64;
+
+
     }
 
 }
+
